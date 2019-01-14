fa0c22d5483d58fbf2e253a99b56524f1d201a00
Peter Mertz <peter.s.mertz@gmail.com>
2019-01-14 14:07:14 -0500

    feat: Add an example of query params (#95)

---
+++ examples/query_params.rs
@@ -0,0 +1,29 @@
+#[macro_use] extern crate thruster;
+extern crate futures;
+extern crate serde_json;
+
+use std::boxed::Box;
+use futures::future;
+use thruster::{App, BasicContext as Ctx, MiddlewareChain, MiddlewareReturnValue, Request};
+use thruster::builtins::server::Server;
+use thruster::builtins::query_params::query_params;
+use thruster::server::ThrusterServer;
+
+fn plaintext(mut context: Ctx, _next: impl Fn(Ctx) -> MiddlewareReturnValue<Ctx>  + Send + Sync) -> MiddlewareReturnValue<Ctx> {
+  let val = serde_json::to_string(&context.query_params).unwrap();
+  context.body(&val);
+
+  Box::new(future::ok(context))
+}
+
+fn main() {
+  println!("Starting server...");
+
+  let mut app = App::<Request, Ctx>::new_basic();
+
+  app.use_middleware("/", middleware![Ctx => query_params]);
+  app.get("/plaintext", middleware![Ctx => plaintext]);
+
+  let server = Server::new(app);
+  server.start("0.0.0.0", 4321);
+}
