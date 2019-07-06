41b2a3386ff4e55376f66b0c9c518838a82cee27
Pete Mertz <peter.s.mertz@gmail.com>
2019-07-06 16:00:12 -0400

    Fix tests to run on stable instead of nightly

--- thruster/src/integration_tests.rs
+++ thruster/src/integration_tests.rs
@@ -1,32 +1,32 @@
-#![feature(async_await, proc_macro_hygiene)]
+use std::boxed::Box;
+use futures::future;
 
-use thruster::{MiddlewareNext, MiddlewareReturnValue, Request};
-use thruster::{App, BasicContext};
-use thruster::thruster_proc::{async_middleware, middleware_fn};
+use thruster::{App, BasicContext as Ctx, MiddlewareChain, MiddlewareReturnValue, Request};
+use thruster_core::{middleware};
 use thruster::testing;
 
-#[middleware_fn]
-async fn test_fn_1(mut context: BasicContext, _next: MiddlewareNext<BasicContext>) -> BasicContext {
+
+fn test_fn_1(mut context: Ctx, _next: impl Fn(Ctx) -> MiddlewareReturnValue<Ctx>  + Send + Sync) -> MiddlewareReturnValue<Ctx> {
   let body = &context.params.get("id").unwrap().clone();
 
   context.body(body);
-  context
+
+  Box::new(future::ok(context))
 }
 
-#[middleware_fn]
-async fn test_fn_404(mut context: BasicContext, _next: MiddlewareNext<BasicContext>) -> BasicContext {
+fn test_fn_404(mut context: Ctx, _next: impl Fn(Ctx) -> MiddlewareReturnValue<Ctx>  + Send + Sync) -> MiddlewareReturnValue<Ctx> {
   context.body("404");
-  context
+
+  Box::new(future::ok(context))
 }
+
 
 #[test]
 fn it_should_correctly_404_if_no_param_is_given() {
-  let mut app = App::<Request, BasicContext>::new_basic();
+  let mut app = App::<Request, Ctx>::new_basic();
 
-  app.get("/test/:id", async_middleware!(BasicContext, [test_fn_1]));
-  app.get("/a", async_middleware!(BasicContext, [test_fn_1]));
-  app.get("/a/b/c", async_middleware!(BasicContext, [test_fn_1]));
-  app.set404(async_middleware!(BasicContext, [test_fn_404]));
+  app.get("/test/:id", middleware![Ctx => test_fn_1]);
+  app.set404(middleware![Ctx => test_fn_404]);
   app._route_parser.optimize();
 
   let response = testing::get(&app, "/test");
