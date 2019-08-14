1f1e4890849512b4a153d4b77149adae0e3a543e
Pete Mertz <peter.s.mertz@gmail.com>
2019-08-14 07:09:23 -0400

    Begin adding SSLServer
    Had to upgrade to tokio-0.2.0-alpha.1 and that's causing a lot of other breakages. Going to have to upgrade eventually any way though!

---
+++ thruster/examples/most_basic_ssl/main.rs
@@ -0,0 +1,34 @@
+#![feature(async_await, proc_macro_hygiene)]
+extern crate thruster;
+
+use std::time::Instant;
+use thruster::{MiddlewareNext, MiddlewareReturnValue};
+use thruster::{App, BasicContext as Ctx, Request};
+use thruster::ssl_server::SSLServer;
+use thruster::ThrusterServer;
+use thruster::thruster_proc::{async_middleware, middleware_fn};
+
+#[middleware_fn]
+async fn plaintext(mut context: Ctx, _next: MiddlewareNext<Ctx>) -> Ctx {
+  let val = "Hello, World!";
+  context.body(val);
+  context
+}
+
+#[middleware_fn]
+async fn test_fn_404(mut context: Ctx, _next: MiddlewareNext<Ctx>) -> Ctx {
+  context.body("404");
+  context
+}
+
+fn main() {
+  println!("Starting server...");
+
+  let mut app = App::<Request, Ctx>::new_basic();
+
+  app.get("/plaintext", async_middleware!(Ctx, [plaintext]));
+  app.set404(async_middleware!(Ctx, [test_fn_404]));
+
+  let server = SSLServer::new(app);
+  server.start_work_stealing_optimized(include_bytes!("identity.p12").to_vec(), "asdfasdfasdf", "0.0.0.0", 4321);
+}
