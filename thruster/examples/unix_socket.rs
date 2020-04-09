fefc0ab9adfb2216917deace301d5c4b8014b58c
Pete Mertz <peter.s.mertz@gmail.com>
2020-04-08 20:16:49 -0400

    feat: Add unix hyper server
    Adds a new server, `UnixHyperServer` that allows users to use local Unix sockets. Resolves [#147](https://github.com/trezm/Thruster/issues/147)

---
+++ thruster/examples/unix_socket.rs
@@ -0,0 +1,27 @@
+use hyper::Body;
+use log::info;
+use thruster::context::basic_hyper_context::{
+    generate_context, BasicHyperContext as Ctx, HyperRequest,
+};
+use thruster::unix_hyper_server::UnixHyperServer;
+use thruster::{async_middleware, middleware_fn};
+use thruster::{App, ThrusterServer};
+use thruster::{MiddlewareNext, MiddlewareResult};
+
+#[middleware_fn]
+async fn plaintext(mut context: Ctx, _next: MiddlewareNext<Ctx>) -> MiddlewareResult<Ctx> {
+    let val = "Hello, World!";
+    context.body = Body::from(val);
+    Ok(context)
+}
+
+fn main() {
+    env_logger::init();
+    info!("Starting server...");
+
+    let mut app = App::<HyperRequest, Ctx>::create(generate_context);
+    app.get("/plaintext", async_middleware!(Ctx, [plaintext]));
+
+
+    UnixHyperServer::new(app).start("/tmp/thruster.sock", 0);
+}
