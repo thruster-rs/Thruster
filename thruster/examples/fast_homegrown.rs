1bf26a0968d12934d3b3bf328d0f422bd4106423
Pete Mertz <peter.s.mertz@gmail.com>
2021-06-02 20:17:43 -0400

    chore: Enable homegrown

---
+++ thruster/examples/fast_homegrown.rs
@@ -0,0 +1,23 @@
+use log::info;
+use thruster::{async_middleware, middleware_fn};
+use thruster::{App, BasicContext as Ctx, Request, Server, ThrusterServer};
+use thruster::{MiddlewareNext, MiddlewareResult};
+
+#[middleware_fn]
+async fn plaintext(mut context: Ctx, _next: MiddlewareNext<Ctx>) -> MiddlewareResult<Ctx> {
+    let val = "Hello, World!";
+    context.body(val);
+    Ok(context)
+}
+
+fn main() {
+    env_logger::init();
+    info!("Starting server...");
+
+    let mut app = App::<Request, Ctx, ()>::new_basic();
+
+    app.get("/plaintext", async_middleware!(Ctx, [plaintext]));
+
+    let server = Server::new(app);
+    server.start_small_load_optimized("0.0.0.0", 4321);
+}
