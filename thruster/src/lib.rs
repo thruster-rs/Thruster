959a02be0b105bf2d68c450634062fb62f80693a
Pete Mertz <peter.s.mertz@gmail.com>
2022-10-14 11:43:45 -0400

    bug: Expose testable

--- thruster/src/lib.rs
+++ thruster/src/lib.rs
@@ -25,6 +25,7 @@ pub use crate::core::response::{encode, Response};
 pub use crate::core::{MiddlewareFn, MiddlewareNext, MiddlewareReturnValue};
 pub use app::testing_async as testing;
 pub use app::App;
+pub use app::Testable;
 
 // Reexport tokio_util::sync::ReusableBoxFuture;
 pub use tokio_util::sync::ReusableBoxFuture;
